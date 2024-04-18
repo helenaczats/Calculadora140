cd578aba7dcce54d736f3aa126067c59b3377549
helenaczats <helenaczats@gmail.com>
2024-04-18 18:48:07 +0200

    Criação de 4 operações aritméticas básicas e testes de unidade simples e data driven

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -11,4 +11,4 @@ def dividir_dois_numeros(num1, num2):
     try:
         return num1 / num2
     except(ZeroDivisionError):
-        return "Erro: Não é possível dividir por zero"
+        return "Não é possível dividir por zero"
